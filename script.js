be8a683a23a92f67de8cef383e800f94a5e0b360
Anish Shenoy <shenoy.anish.227@gmail.com>
2014-12-15 12:15:37 -0500

    Added stop

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 var div = null;
 var timerToggle = null;
 var startStop = null;
+var choice = "stop";
+
+function choose () {
+  if(choice == "stop") {
+    choice = "start";
+	}
+  else{
+    choice = "stop";
+	}
+  init();
+}
+    
+
+
 
 function moveRight() {
-  div.style.left = parseInt(div.style.left)+1+'px';
+  if(choice == "start") {
+  div.style.left = parseInt(div.style.left)+2+'px';
   timerToggle = setTimeout(moveRight,80);
   }
+  else {
+    window.clearTimeout(timerToggle);
+	}
+}
   
 function init() {
   div = document.getElementById("message");
