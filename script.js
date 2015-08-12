e8fc8a6f1a322aca3cccf2fb2e32869b6e8d6702
Anish2454 <shenoy.anish.227@gmail.com>
2015-08-12 10:17:41 -0400

    Fixed animation Bug

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function moveRight() {
     div.style.left = parseInt(div.style.left)+2+'px';
     timerToggle = setTimeout(moveRight, 20);
 	
-	if (parseInt(div.style.left) > 800) {
+	if (parseInt(div.style.left) > 1500) {
 	    window.clearInterval(timerToggle);
         moveLeft();
 	}
